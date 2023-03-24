ddda23aaba722a8c634552dce98c8157b91904f5
Brian Bentancourt <brianbentancourt9@gmail.com>
2023-03-24 18:49:49 -0300

    project card

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,7 +1,44 @@
+import {
+    Grid,
+    Avatar,
+    Typography
+} from '@mui/material';
+import ProjectCard from '../ProjectCard'
+import { red } from '@mui/material/colors';
+import { data } from '../../data/projects.js'
+import Title from '../Title';
 
-
-export default function Projects() {
+export default function Projects({ isBiggerThanMd }) {
     return (
-        <div>Projects</div>
+        <Grid
+            container
+            spacing={5}
+            justifyContent="space-around"
+            alignItems="center"
+        // style={{ paddingLeft: isBiggerThanMd ? 200 : 45, paddingTop: 70, paddingRight: 20 }}
+        >
+            <Grid item xs={12}>
+                <Title>My Freelance Projects</Title>
+            </Grid>
+            {
+                data?.map((info, index) =>
+                    <Grid
+                        key={index}
+                        item
+                        xs={12}
+                        sm={6}
+                        md={5}
+                        lg={4}
+                        container
+                        justifyContent="center"
+                        alignItems="center"
+                        style={{ marginTop: 50 }}
+                    >
+                        <ProjectCard info={info} />
+                    </Grid>
+                )
+            }
+
+        </Grid>
     )
 }
