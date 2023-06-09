01979bcd25f93aa573b2e5ed3d5135dced6847eb
Brian Bentancourt <brian.bentancourt@sonda.com>
2023-06-09 10:40:36 -0300

    client feedback

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,4 +1,5 @@
 import {
+    Grid,
     Avatar,
     Card,
     CardContent,
@@ -10,24 +11,27 @@ import {
 export default function CommentCard({ item }) {
     return (
         <Card>
-            <CardHeader
-                avatar={
-                    <Avatar sx={{
-                        width: 60,
-                        height: 60,
-                        bgcolor: 'red',
-                        marginLeft: -4,
-                        marginTop: -5,
-                        position: 'absolute'
-                    }}
-                        aria-label="recipe"
-                    >
-                        R
-                    </Avatar>
-                }
-            />
             <CardContent>
-                <Typography variant='h5' component='div' style={{ marginBottom: 20 }}>{item.name}</Typography>
+                <Avatar sx={{
+                    width: 90,
+                    height: 90,
+                    marginLeft: -3.5,
+                    marginTop: -3.5,
+                    position: 'absolute'
+                }}
+                    aria-label="recipe"
+                    src={item.logo.src}
+                >
+                </Avatar>
+                <Grid container style={{ marginLeft: 70, marginBottom: 20 }}>
+                    <Grid item xs={12}>
+                        <Typography variant='h5' color="primary" component='div'  >{item.name}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='9' component='div' color="text.secondary" style={{ fontSize: 14 }}>{item.job}</Typography>
+                    </Grid>
+                </Grid>
+
                 <Typography variant="body2">
                     {item.comment}
                 </Typography>
---
+++ src/data/clients.js
@@ -0,0 +1,20 @@
+import agustyleLogo from '../images/clients/agustyle.png'
+import befeburgersLogo from '../images/clients/befeburgers.jpg'
+
+export const clients = [
+    {
+        id: 1,
+        name: 'Agustin Ceballos',
+        job: 'Owner at Agustyle',
+        logo: agustyleLogo,
+        comment: 'Mi nombre es Agustín y me dedico a trabajar en el área de estética masculina más precisamente en el corte de cabello y barba, llevo más o menos 5 años en el rubro y hace aproximadamente 3 años atrás, me ví obligado por causa de la pandemia a organizar mi clientela, hasta el momento trabajaba por orden de llegada, llegando a tener en el local de 5 a 8 personas esperando para ser atendidos, comencé a usar métodos como reservas por WhatsApp, pero aún así, no encontraba la solución para lograr un orden y una eficiencia, ya que tenía que estar atento al teléfono y a la ves atendiendo a los clientes, Gracias a Brian Bentancourt encontré la solución a mi problema, me diseño una aplicación de agenda web por la cual los clientes se pueden agrandar ellos mismos, y así yo poder trabajar tranquilo y no estar pendiente al teléfono, hoy día ya a un poco más de 1 año pasada la pandemia sigo trabajando de la misma manera, ya que la aplicación web tuvo muy buena aceptación por los clientes y resultó una herramienta de solución para mí, siendo también una novedad en mi rubro.'
+    },
+    {
+        id: 2,
+        name: 'Bruno Fiorelli',
+        job: 'Owner at Befe Burgers',
+        logo: befeburgersLogo,
+        comment: `Como reseña al servicio que brinda Brian, debo decir que en nuestro caso gestionó un programa que optimizó el trabajo laboral y la atención al cliente. Mejoramos en varios ambitos gracias al sistema!
+        La atención personal de Brian siempre fue excelente también, solucionado dudas o corrigiendo detalles lo más rápido posible.`
+    },
+]
