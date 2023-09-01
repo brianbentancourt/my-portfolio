cbf4915f327b57c77878c1ffa43452c9ba04565b
Brian Bentancourt <brian.bentancourt@sonda.com>
2023-09-01 12:15:40 -0300

    showing feedback on load courses images

--- src/pages/courses/index.js
+++ src/pages/courses/index.js
@@ -2,21 +2,30 @@ import { useEffect, useState } from "react";
 import Title from "@/components/Title";
 import {
     Grid,
+    Snackbar,
+    Backdrop,
+    CircularProgress
 } from "@mui/material";
 import { listAll, getDownloadURL, ref } from 'firebase/storage'
 import { storage } from "@/firebase/config";
 import CoursesImage from "@/components/CoursesImage";
 import CourseModal from "@/components/CourseModal";
+import Alert from "@/components/Alert";
 
 
 export default function Courses() {
     const [courses, setCourses] = useState([]);
     const [selectedCourse, setSelectedCourse] = useState({})
     const [courseOpen, setCourseOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [notificationOpen, setNotificationOpen] = useState(false)
+    const [notificationMessage, setNotificationMessage] = useState('')
+    const [notificationSeverity, setNotificationSeverity] = useState('success')
 
     useEffect(() => {
         const fetchCourseDataFromFirebaseStorage = async () => {
             try {
+                setLoading(true)
                 const coursesRef = ref(storage, '/courses')
                 const courseList = await listAll(coursesRef);
 
@@ -30,8 +39,13 @@ export default function Courses() {
 
                 const courseData = await Promise.all(promises);
                 setCourses(courseData);
+                setLoading(false)
             } catch (error) {
                 console.log(error);
+                setNotificationSeverity('error')
+                setNotificationMessage('An error occurred, please try again')
+                setNotificationOpen(true)
+                setLoading(false)
             }
         };
 
@@ -52,6 +66,17 @@ export default function Courses() {
                 padding: '5vw'
             }}
         >
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <Snackbar open={notificationOpen} autoHideDuration={6000} onClose={() => setNotificationOpen(false)}>
+                <Alert onClose={() => setNotificationOpen(false)} severity={notificationSeverity} sx={{ width: '100%' }}>
+                    {notificationMessage}
+                </Alert>
+            </Snackbar>
             <CourseModal open={courseOpen} setOpen={setCourseOpen} course={selectedCourse} />
             <Grid item xs={12} style={{ padding: '0 15vw' }}>
                 <Title>Courses</Title>
